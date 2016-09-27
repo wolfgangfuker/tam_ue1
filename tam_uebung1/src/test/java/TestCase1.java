2999509b2653460858fec560dcd3774afdbbb0f4
wolfgangfuker <wolfgang.fuker@gmail.com>
2016-09-27 19:51:13 +0200

    improved Testassertion

--- tam_uebung1/src/test/java/TestCase1.java
+++ tam_uebung1/src/test/java/TestCase1.java
@@ -8,76 +8,77 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.fail;
 
 public class Testcase1 {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://127.0.0.1/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://127.0.0.1/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-  }
 
-  @Test
-  public void whenBuyingOneBook_thenOrderIsProvisioned() throws Exception {
-    driver.get(baseUrl + "/warehouse/WareHouse.php");
-    driver.findElement(By.name("Query_types")).click();
-    driver.findElement(By.id("UserCode")).clear();
-    driver.findElement(By.id("UserCode")).sendKeys("10000");
-    new Select(driver.findElement(By.name("ArticleType"))).selectByVisibleText("BOOK");
-    driver.findElement(By.name("Query_articles")).click();
-    driver.findElement(By.id("c1")).click();
-    driver.findElement(By.id("n1")).clear();
-    driver.findElement(By.id("n1")).sendKeys("1");
-    driver.findElement(By.name("Buy_articles")).click();
-    assertThat(isElementPresent(By.xpath("//tr[6]/td"))).isTrue();
-    assertThat("Orders fulfilled = 0 Response #:1000").isEqualTo(driver.findElement(By.xpath("//tr[6]/td")).getText());
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    @Test
+    public void whenBuyingOneBook_thenOrderIsProvisioned() throws Exception {
+        driver.get(baseUrl + "/warehouse/WareHouse.php");
+        driver.findElement(By.name("Query_types")).click();
+        driver.findElement(By.id("UserCode")).clear();
+        driver.findElement(By.id("UserCode")).sendKeys("10000");
+        new Select(driver.findElement(By.name("ArticleType"))).selectByVisibleText("BOOK");
+        driver.findElement(By.name("Query_articles")).click();
+        driver.findElement(By.id("c1")).click();
+        driver.findElement(By.id("n1")).clear();
+        driver.findElement(By.id("n1")).sendKeys("1");
+        driver.findElement(By.name("Buy_articles")).click();
+        assertThat(isElementPresent(By.xpath("//tr[6]/td"))).isTrue();
+        assertThat(driver.findElement(By.xpath("//tr[6]/td")).getText()).contains("Orders fulfilled =", "Response #:");
+//        assertThat("Orders fulfilled = 0 Response #:1000").isEqualTo(driver.findElement(By.xpath("//tr[6]/td")).getText());
     }
-  }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-  }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
